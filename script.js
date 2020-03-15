73e95c6d580622b74c5393a93999db139f2332fe
Huelsgoat <huntsperger@gmail.com>
2020-03-15 09:38:31 -0700

    working better, still not pretty

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // When start is pressed, hide jumbo, show first question
-
+var bigBucket = document.querySelector(".container");
 var intro = document.querySelector(".poof");
 var startButt = document.querySelector("#startButton");
 var theQuiz = document.querySelector(".quizbucket");
 var quizList = document.querySelector(".quizlist");
 var submitButt = document.querySelector("#answersubmit");
 var theScore = document.querySelector("#score");
+var scoreDiv = document.querySelector("#scorediv");
+var scoreBuck = document.querySelector(".scorebucket");
 var i = 0;
-// submitButt.setAttribute("class", "away");
-submitButt.remove();
+var scoreCount = 0;
+var z = 0;
+scoreDiv.remove();
+
 var questionsOb = [
     {
         question: "What is a Japanese chef's knife called?",
@@ -51,52 +55,18 @@ var questionsOb = [
         correct: 2
     }];
 
-// var questions = [
-//     [
-//         "What is a Japanese chef's knife called?",
-//         "a: Scimitar",
-//         "b: Gyuto",
-//         "c: Wusthof",
-//         "d: Bowie Knife"
-
-//     ],
-//     [
-//         "What is a good, usable length for a chef's knife blade?",
-//         "a: 20mm - 30mm  ",
-//         "b: 400mm - 600mm  ",
-//         "c: 800mm - 1000mm  ",
-//         "d: 180mm - 270mm  "
-
-//     ],
-//     [
-//         "What is a good tool for sharpening a knife?",
-//         "a: Waterstone",
-//         "b: Cat",
-//         "c: Golf ball",
-//         "d: Piece of wood",
-//     ],
-//     [
-//         "What is a Japanese vegetable knife called?",
-//         "a: Narwhal",
-//         "b: Nutcracker",
-//         "c: Nakiri",
-//         "d: Nissan"
-
-//     ]
-// ]
-
-
 
 // Each time a question is answered, show next question
 // TODO: need to figure out how to look through the object or just use strings - still need loop though
 function makeQuiz() {
-    // submitButt.removeAttribute("class", "away");
-    // submitButt.setAttribute("class", "btn btn-primary btn-lg");
-    theQuiz.append(submitButt);
+    
+
+    
     quizList.textContent = "";
 
     if (i < questionsOb.length) {
         var newDiv0 = document.createElement("div");
+        // var current = questionsOb[i].correct;
         newDiv0.innerHTML = questionsOb[i].question;
         quizList.appendChild(newDiv0);
 
@@ -105,6 +75,7 @@ function makeQuiz() {
         for (var y = 0; y < questionsOb[i].answers.length; y++) {
             var newDiv = document.createElement("div");
             newDiv.innerHTML = questionsOb[i].answers[y];
+            newDiv.setAttribute("data-index", y);
             var button = document.createElement("button");
             // gotta figure out how to keep score here. radio buttons maybe????
             button.setAttribute("class", "btn btn-light btn-sm ml-2");
@@ -115,44 +86,50 @@ function makeQuiz() {
 
         }
     } else {
-        submitButt.setAttribute("class", "away");
+        var initials = prompt("what are your initials?");
+        theScore.textContent = `Score: ${initials} - ${scoreCount}`;
+        scoreBuck.appendChild(theScore);
+        storeScore();
+        bigBucket.append(scoreDiv);
+    }
+    
+
+
+    i++;
+    
+}
+    startButt.addEventListener("click", function () {
+        intro.setAttribute("class", "away");
+        makeQuiz();
+    })
+
+    function storeScore() {
+        localStorage.setItem("scoreCount", JSON.stringify(scoreCount));
     }
 
-    button.addEventListener("click", function(){
-        console.log(event.target);
-    })
-    // if (i < questions.length) {
-    //     for (var y = 0; y < questions[i].length; y++) {
-    //         var newDiv = document.createElement("div");
-    //         newDiv.innerHTML = questions[i][y];
-    //         quizList.appendChild(newDiv);
+    
+    function tallyScore(what) {
+        var rightAns = questionsOb[z].correct;
+        var userAns = parseInt(what);
+        if (userAns === rightAns) {
+            scoreCount++;
+        }
+        
 
-    //     }
-    // }
+        z++;
+    }
+    
 
-    // if (i < questions.length) {
-    // newPara.innerHTML = questions[i];
-    // this might work, but i'm probably just overwriting the variable. probably need to plop it into an array and then loop through it on output.
-    // also, might be able to track this with a data-value and get each one that way...
-    // for (var index = 0; index < questionsOb[i].length; index++) {
-    // var newPara2 = document.createElement("li");
-    // newPara2.innerHTML = questionsOb[i][index];
-    // }
-    // };
+    quizList.addEventListener("click", function(){
 
-    // quizList.append(newPara);
-    // quizList.append(newPara2);
-    i++;
-
-}
-startButt.addEventListener("click", function () {
-    intro.setAttribute("class", "away");
-    makeQuiz();
-})
-
-submitButt.addEventListener("click", function () {
-    makeQuiz()
-})
+        var element = event.target;
+        if (element.matches("button") === true) {
+            var index = element.parentElement.getAttribute("data-index");
+            // console.log(index);
+            tallyScore(index);
+            makeQuiz();
+        }
+    }); 
 
 
 // Make timer begin on start button press
